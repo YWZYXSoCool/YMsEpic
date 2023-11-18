1d673686eb00fa69c01daa72281716eda09ae6b4
lj su <ywzyxsocool@qq.com>
2023-11-18 21:19:17 +0800

    a

---
+++ src/main/java/com/example/examplemod/util/RegistryHandler.java
@@ -0,0 +1,21 @@
+package com.example.examplemod.util;
+
+import com.example.examplemod.ExampleMod;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class RegistryHandler {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS,
+            ExampleMod.MOD_ID);
+
+    public static void init() {
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+
+    public static final RegistryObject<Item> ITEM_CRYSTAL = ITEMS.register("crystal",
+            () -> new Item(new Item.Properties()));
+}
